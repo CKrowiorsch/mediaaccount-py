a19c0a6ed19bc53c5b35c6c479fb0ec344597cba
Christian Krowiorsch <christian.krowiorsch@googlemail.com>
2021-08-19 18:42:39 +0200

    Erweiterung für Tests

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import sys,getopt
         
 def main(argv):
     verbose = False
-    apiKey = None   
+    apiKey = None
+    all = False
 
     try:
-        opts, args = getopt.getopt(argv,"hk:",["apikey=","verbose"])
+        opts, args = getopt.getopt(argv,"hk:",["apikey=","verbose", "all"])
     except getopt.GetoptError:
         print('error: app.py --apikey <apikey> --verbose')
         sys.exit(2)
@@ -19,15 +20,27 @@ def main(argv):
             apiKey = arg
         elif opt in ("--verbose"):
             verbose = True
+        elif opt in ("--all"):
+            all = True
 
     client = MediaAccountClient(apiKey)
 
+    if (all):
+        scroll = client.scroll('ImportDatum', von = '04.08.2021', bis = '05.08.2021', maxItems=1000)
+
+        currentIndex = 0
+        for article, count in scroll:
+            currentIndex += 1
+            print(f'{currentIndex}/{count}: {article.inhalt.headline}')
+
+        sys.exit()
 
     print(f'Request with ApiKey:{apiKey}')
-    articles, nextlink, count = client.articles('ImportDatum', von = '01.01.2021', bis = '05.08.2021')
+    articles, nextlink, count = client.articles('ImportDatum', von = '01.05.2021', bis = '05.08.2021')
 
     print(f'{count} Gesamt')
     print(f'{len(articles)} Artikel empfangen ')
+    print(f'nextlink: {nextlink} ')
 
     if (verbose):
         for article in articles:
